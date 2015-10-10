b8dc7333e615a9ad2623a863afdd6c0ceeb100bd
lielc <zaboolina@gmail.com>
2015-10-10 21:44:57 +0300

    initialize

---
+++ WebAppProj/WebAppProj/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebAppProj.Startup))]
+namespace WebAppProj
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
