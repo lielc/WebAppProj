d3f865c2253085ebdde22278e9941bc204bedde5
shaniavnaim <shaniavnaim@gmail.com>
2015-10-11 20:02:34 +0300

    New Proj

---
+++ WebAppProj/WebAppProj/Models/lecture.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+
+namespace WebAppProj.Models
+{
+    public class lecture
+    {
+        [Key]
+        public long lectureId { get; set; }
+        public string name { get; set; }
+        public string location { get; set; }
+        [ForeignKey("instructor"), Column("instructorId")]
+        public string instructorId { get; set; }
+        public DateTime startDate { get; set; }
+        public DateTime endDate { get; set; }
+
+        public virtual instructor instructor { get; set; }
+    }
+    public class lectureDb : DbContext
+    {
+        public DbSet<lecture> lectures { get; set; }
+    }
+}
--- WebAppProj/WebAppProj/Models/user.cs
+++ WebAppProj/WebAppProj/Models/user.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Data.Entity;
 using System.Linq;
 using System.Web;
@@ -11,18 +12,14 @@ namespace WebAppProj.Models
     {
         [Key]
         public long userId { get; set; }
-
         public string firstName { get; set; }
-
         public string lastName { get; set; }
-
         public string email { get; set; }
-
         public string phoneNum { get; set; }
-
-        public string password { get; set; }
-
-        public string address { get; set; }
+        [ForeignKey("lectuer"), Column("lectuerId")]
+        public long classId { get; set; }
+        public string password { get; set; }      
+        public virtual lecture lectuer { get; set; }
     }
     public class usersDb : DbContext
     {
